f727429819040e8113eda0166dd69e0b2e2917a8
Greg Lauckhart <greg@lauckhart.com>
2023-05-13 22:06:24 -0700

    Forgot this earlier

--- packages/matter-crypto.js/src/implementations/CryptoJS.ts
+++ packages/matter-crypto.js/src/implementations/CryptoJS.ts
@@ -16,8 +16,12 @@ function bytesToBuf(bytes: ByteArray) {
 
 function bytesToBits(bytes: ByteArray) {
     return sjcl.codec.bytes.toBits(<number[]><unknown>bytes);
+    return sjcl.codec.bytes.toBits(<number[]><unknown>bytes);
 }
 
+//function bitsToBytes(bits: sjcl.BitArray) {
+//    return new ByteArray(sjcl.codec.arrayBuffer.fromBits(bits));
+//}
 //function bitsToBytes(bits: sjcl.BitArray) {
 //    return new ByteArray(sjcl.codec.arrayBuffer.fromBits(bits));
 //}
@@ -43,6 +47,7 @@ const ccmEncrypt = <(
 /**
  * This class implements matter crypto using pure JavaScript.  It may be
  * completed in the future but as of right now just contains portions required
+ * by CryptoSubtle to cover gaps in Web Crypto.
  * by CryptoSubtle to cover gaps in Web Crypto.
  */
 export abstract class CryptoJS extends Crypto {
@@ -82,6 +87,9 @@ export abstract class CryptoJS extends Crypto {
     override decrypt(key: ByteArray, data: ByteArray, nonce: ByteArray, aad?: ByteArray) {
         const length = data.length - CRYPTO_AUTH_TAG_LENGTH;
         if (data.length < 0) {
+    override decrypt(key: ByteArray, data: ByteArray, nonce: ByteArray, aad?: ByteArray) {
+        const length = data.length - CRYPTO_AUTH_TAG_LENGTH;
+        if (data.length < 0) {
             throw new Error("Invalid ciphertext");
         }
 
@@ -94,15 +102,28 @@ export abstract class CryptoJS extends Crypto {
         const workBytes = new ByteArray(workBuf);
         workBytes.set(data.slice(CRYPTO_AUTH_TAG_LENGTH));
 
+        // Extract the tag
+        const tag = bytesToBits(new ByteArray(data.slice(0,  CRYPTO_AUTH_TAG_LENGTH)));
+
+        // Extract the cyphertext
+        const padding = key.length % CCM_BLOCK_SIZE ? CCM_BLOCK_SIZE - (key.length % CCM_BLOCK_SIZE) : 0;
+        const workBuf = new ArrayBuffer(data.length - CRYPTO_AUTH_TAG_LENGTH + padding);
+        const workBytes = new ByteArray(workBuf);
+        workBytes.set(data.slice(CRYPTO_AUTH_TAG_LENGTH));
+
+        // Decrypt
         // Decrypt
         sjcl.arrayBuffer.ccm.decrypt(
             aesForKey(key),
+            workBuf,
             workBuf,
             bytesToBits(nonce),
             tag,
             (aad && bytesToBuf(aad)
         ));
 
+        // Remove padding (resize not supported everywhere)
+        return workBytes.slice(0, length);
         // Remove padding (resize not supported everywhere)
         return workBytes.slice(0, length);
     }
