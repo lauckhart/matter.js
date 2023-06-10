9cd91c489f8dcb98efee3fa7b451669725ae8ee5
Greg Lauckhart <greg@lauckhart.com>
2023-06-10 00:50:31 -0700

    Updating aspect tests

---
+++ packages/matter.js/test/model/aspects/ConformanceTest.ts
@@ -0,0 +1,47 @@
+/**
+ * @license
+ * Copyright 2022-2023 Project CHIP Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { Conformance } from "../../../src/model/aspects/Conformance.js";
+
+const TEST_DEFINITIONS = [
+    "M",
+    "O",
+    "P",
+    "D",
+    "X",
+    "WBL",
+    "AX | WBL",
+    "AX, WBL",
+    "[WIRED]",
+    "!AB",
+    "AB.a",
+    "AB.a+",
+    "AB.a2",
+    "AB.a2+",
+    "AB == 2"
+]
+
+const TEST_DEFINITIONS2 = {
+    "(AX | WBL)": "AX | WBL"
+}
+
+function testOne(definition: string, expected = definition) {
+    describe(definition, () => {
+        it("parses", () => {
+            expect(() => new Conformance(definition)).not.toThrow();
+        })
+
+        it("serializes", () => {
+            const conformance = new Conformance(definition);
+            expect(`${conformance}`).toBe(expected);
+        })
+    })
+}
+
+describe("Conformance", () => {
+    TEST_DEFINITIONS.forEach(d => testOne(d));
+    Object.entries(TEST_DEFINITIONS2).forEach(([ d, e ]) => testOne(d, e));
+})
