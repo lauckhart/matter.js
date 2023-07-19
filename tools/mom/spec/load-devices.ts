236c1d03e85cdc39b04aafa8a940ab51e6358d22
Greg Lauckhart <greg@lauckhart.com>
2023-07-18 23:25:19 -0500

    Initial device ingestion

--- tools/mom/spec/load-devices.ts
+++ tools/mom/spec/load-devices.ts
@@ -4,12 +4,51 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-//import { scanSection } from "./scan-section.js";
-import { HtmlReference } from "./spec-types.js";
+import { NavigateViaNav, scanSection } from "./scan-section.js";
+import { DeviceReference, HtmlReference } from "./spec-types.js";
 
-// eslint-disable-next-line require-yield
-export function* loadDevices(devices: HtmlReference): Generator<HtmlReference> {
-    // TODO
-    devices;
-    //scanSection(devices)
+export function* loadDevices(devices: HtmlReference) {
+    let category: string | undefined;
+    let device: DeviceReference | undefined;
+
+    function* emit() {
+        if (device) {
+            yield device;
+            device = undefined;
+        }
+    }
+
+    function add(ref: HtmlReference) {
+        // TODO
+        ref;
+    }
+
+    for (const section of scanSection(devices, NavigateViaNav)) {
+        const depth = section.xref.section.split(".").length;
+        
+        switch (depth) {
+            case 1:
+                yield* emit();
+                category = section.xref.section.replace(/\s+device types?/i, "");
+                break;
+
+            case 2:
+                yield* emit();
+                
+                if (section.name.match(/\s+architecture$/i)) {
+                    break;
+                }
+
+                device = {
+                    ...section,
+                    name: section.name.replace(/\s+device type/i, ""),
+                    category
+                }
+                break;
+
+            default:
+                add(section);
+                break;
+        }
+    }
 }
