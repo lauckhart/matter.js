9427c45dddb2039d34a18a374379b59df1ce2094
Greg Lauckhart <greg@lauckhart.com>
2023-07-18 23:25:19 -0500

    Initial device ingestion

--- tools/generate-spec.ts
+++ tools/generate-spec.ts
@@ -18,7 +18,7 @@
 // mention the term "masochist".
 
 import "./util/setup.js";
-import { ClusterElement } from "#matter.js/model/index.js"
+import { MatterElement } from "#matter.js/model/index.js"
 import { scanIndex } from "./mom/spec/scan-index.js";
 
 import { paths } from "./mom/spec/spec-input.js";
@@ -30,7 +30,7 @@ import { generateIntermediateModel } from "./mom/common/generate-intermediate.js
 import { loadDevices } from "./mom/spec/load-devices.js";
 import { translateDevice } from "./mom/spec/translate-device.js";
 
-const clusters = Array<ClusterElement>();
+const elements = Array<MatterElement.Child>();
 const logger = Logger.get("generate-spec");
 
 function scanCluster(clusterRef: HtmlReference) {
@@ -42,14 +42,14 @@ function scanCluster(clusterRef: HtmlReference) {
         Logger.nest(() => definition = loadCluster(clusterRef));
 
         logger.info("translate");
-        Logger.nest(() => clusters.push(...translateCluster(definition)));
+        Logger.nest(() => elements.push(...translateCluster(definition)));
     });
 }
 
 function scanDevices(devices: HtmlReference) {
     for (const deviceRef of loadDevices(devices)) {
         logger.info(`device ${deviceRef.name} (${deviceRef.xref.document} § ${deviceRef.xref.section})`);
-        Logger.nest(() => translateDevice(deviceRef));
+        Logger.nest(() => elements.push(...translateDevice(deviceRef)));
     }
 }
 
@@ -57,11 +57,11 @@ paths.forEach(path => {
     logger.info(`load from spec ${path}`);
     Logger.nest(() => {
         const index = scanIndex(path);
-        index.clusters.forEach(scanCluster);
+        scanCluster; //index.clusters.forEach(scanCluster);
         if (index.device) {
             scanDevices(index.device);
         }
     });
 });
 
-generateIntermediateModel("spec", clusters);
+generateIntermediateModel; //generateIntermediateModel("spec", elements);
