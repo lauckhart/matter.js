27258ee7ea2642824806377d46748ffe09bf684e
Greg Lauckhart <greg@lauckhart.com>
2024-10-24 11:17:10 -0700

    Address PR feedback

--- packages/matter.js/src/MatterController.ts
+++ packages/matter.js/src/MatterController.ts
@@ -346,7 +346,7 @@ export class MatterController {
         };
 
         if (completeCommissioningCallback) {
-            commissioningOptions.performCaseCommissioning = async (peerAddress, discoveryData) => {
+            commissioningOptions.finalizeCommissioning = async (peerAddress, discoveryData) => {
                 const result = await completeCommissioningCallback(peerAddress.nodeId, discoveryData);
                 if (!result) {
                     throw new RetransmissionLimitReachedError("Device could not be discovered");
